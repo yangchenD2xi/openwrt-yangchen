ea094bebfd14d81fa9e6fdbda39c2073e3f06bd7
yangchenD2xi <yangchend2xi@163.com>
2024-12-03 22:46:08 +0800

    Update 1.clone.sh

--- preset-lienol-openwrt/1.clone.sh
+++ preset-lienol-openwrt/1.clone.sh
@@ -14,6 +14,7 @@ mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/Ausaci/luci-app-nat6-helper.git
+git clone --depth 1 https://github.com/kenzok8/small-package.git
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git && mv openwrt-passwall2/luci-app-passwall2 ./ && rm -rf openwrt-passwall2
